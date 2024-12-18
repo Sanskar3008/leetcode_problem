96a40b25e905bcb8665c287bf551832334e77b15
Sanskar30 <125880199+Sanskar3008@users.noreply.github.com>
2024-12-18 23:11:39 +0530

    Time: 2 ms (7.95%), Space: 43.9 MB (7.34%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -6,7 +6,9 @@ class Solution {
             Arrays.fill(t[i],-1);
         }
         
-        return  solve(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1);
+       // return  solve(obstacleGrid,obstacleGrid.length-1,obstacleGrid[0].length-1);
+        return bottomup(obstacleGrid);
+
     }
 
     public int solve(int [][]arr,int i,int j)
@@ -30,5 +32,42 @@ class Solution {
 
         return t[i][j]=l+r;
 
+    }
+
+    public int bottomup(int arr[][])
+    {
+        int dp[][]=new int[arr.length+1][arr[0].length+1];
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[0].length;j++)
+            {
+                if(arr[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0&&j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+           int l=0;
+           int r=0;
+                if(i>0 &&dp[i][j]!=1)
+                {
+                   l=dp[i-1][j];
+                }
+
+                
+                if(j>0 &&dp[i][j]!=1)
+                {
+                   r=dp[i][j-1];
+                }
+               dp[i][j]=l+r;
+            }
+        }
+
+       return dp[arr.length-1][arr[0].length-1];
     }
 }
