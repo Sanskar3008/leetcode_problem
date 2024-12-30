87f842f76769e4a2e8d3566d7e86e3c94711b04d
Sanskar30 <125880199+Sanskar3008@users.noreply.github.com>
2024-12-30 20:00:19 +0530

    Time: 8 ms (6.07%), Space: 45.1 MB (99.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,7 +6,8 @@ class Solution {
             Arrays.fill(t[i],-1);
         }
     
-        return solve(grid,grid.length-1,grid[0].length-1);
+        //return solve(grid,grid.length-1,grid[0].length-1);
+        return solve2(grid);
         
     }
       public int solve(int [][]arr,int i,int j)
@@ -30,5 +31,32 @@ class Solution {
 
         return t[i][j]=Math.min(l,r);
 
+    }
+
+    public int solve2(int [][]arr)
+    {
+        int m = arr.length; // Number of rows
+    int n = arr[0].length; // Number of columns
+
+    int[][] dp = new int[m][n]; // Initialize DP table with the same size as arr
+
+    // Iterate through the grid
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                // Starting cell
+                dp[i][j] = arr[i][j];
+            } else {
+                // Minimum path sum from the top or left
+                int fromTop = (i > 0) ? dp[i - 1][j] : Integer.MAX_VALUE;
+                int fromLeft = (j > 0) ? dp[i][j - 1] : Integer.MAX_VALUE;
+                dp[i][j] = arr[i][j] + Math.min(fromTop, fromLeft);
+            }
+        }
+    }
+
+    // The bottom-right cell contains the minimum path sum
+    return dp[m - 1][n - 1];
+        
     }
 }
